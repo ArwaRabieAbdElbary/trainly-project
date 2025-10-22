17fcc9e2e4ef3e9c67f2a2d109c9a42d845b6566
Arwa Rabie <arwarabie295@gmail.com>
2025-10-22 23:56:37 +0300

    dashboardchange

--- src/pages/trainer/TrainerDashboardLayout.vue
+++ src/pages/trainer/TrainerDashboardLayout.vue
@@ -2,7 +2,7 @@
   <div class="flex">
     <SideBar />
 
-    <div class="flex-1 ml-64 p-10 min-h-screen">
+    <div class="flex-1 lg:ml-64 p-10 min-h-screen">
       <router-view />  
     </div>
   </div>
